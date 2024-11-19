5dc2c758814be3d8c234186389bf71ee36383695
Vishal Chand <sam.chand96@gmail.com>
2024-11-19 14:58:34 +0530

    First react native project: Simple Hello world app, that can change the background and text color based on the theme selected by user.

---
+++ AppPro.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+
+function AppPro(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView
+      style={isDarkMode ? styles.safeAreaDark : styles.safeAreaLight}>
+      <View style={styles.container}>
+        <Text style={isDarkMode ? styles.textWhite : styles.textDark}>
+          Hello World!
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  safeAreaLight: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  safeAreaDark: {
+    flex: 1,
+    backgroundColor: '#000000',
+  },
+  textWhite: {
+    color: '#FFFFFF',
+  },
+  textDark: {
+    color: '#000000',
+  },
+});
+
+export default AppPro;
